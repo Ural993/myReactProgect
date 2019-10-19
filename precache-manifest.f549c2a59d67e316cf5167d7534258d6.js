b23aec08affcb9eef2cfc729833573b104714d5f
Ural Islamov <ural-islamov@mail.ru>
2019-10-19 23:02:02 +0500

    Updates

---
+++ precache-manifest.f549c2a59d67e316cf5167d7534258d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662b5ed098722728fe5c184355e009b9",
+    "url": "/myReactProgect/index.html"
+  },
+  {
+    "revision": "f44e548b10849998f323",
+    "url": "/myReactProgect/static/css/main.f9aef9c8.chunk.css"
+  },
+  {
+    "revision": "72674bc2bb2b5ff8dc46",
+    "url": "/myReactProgect/static/js/2.e07caefd.chunk.js"
+  },
+  {
+    "revision": "f44e548b10849998f323",
+    "url": "/myReactProgect/static/js/main.acbcb7da.chunk.js"
+  },
+  {
+    "revision": "605922ff50e9000012a0",
+    "url": "/myReactProgect/static/js/runtime~main.34993ad2.js"
+  },
+  {
+    "revision": "83c539b17a34690133d4edb00599fcc2",
+    "url": "/myReactProgect/static/media/loader.83c539b1.svg"
+  }
+]);
